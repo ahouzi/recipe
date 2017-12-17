222c52dac825d0b8dc52a7e5ea3c6690a14a4fb3
aah <mr.ahouzi@gmail.com>
2017-12-17 10:38:35 +0000

    create recipe

--- src/main/java/com/recipe/project/controllers/RecipeController.java
+++ src/main/java/com/recipe/project/controllers/RecipeController.java
@@ -1,10 +1,13 @@
 package com.recipe.project.controllers;
 
+import com.recipe.project.commands.RecipeCommand;
 import com.recipe.project.services.RecipeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -18,10 +21,21 @@ public class RecipeController {
 
     @RequestMapping("/recipe/show/{id}")
     public String getRecipe(Model model, @PathVariable String id ) {
-
         model.addAttribute("recipe",recipeService.getRecipeById(Long.valueOf(id)));
-
         return "recipe/show";
+    }
+
+    @RequestMapping("/recipe/new")
+    public String newRecipe(Model model){
+        model.addAttribute("recipeForm", new RecipeCommand());
+        return "recipe/recipeForm";
+    }
+
+    @PostMapping
+    @RequestMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand recipeCommand){
+        RecipeCommand savedRecipe = recipeService.saveRecipeCommand(recipeCommand);
+        return "redirect:/recipe/show/" + savedRecipe.getId();
     }
 
 }
